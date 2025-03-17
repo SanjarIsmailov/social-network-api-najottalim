10ae3f2f054e4476e6b7897a9d3a6bdc19714e75
sanjar <SanjarIsmailov@users.noreply.github.com>
2025-03-17 19:24:27 +0500

    1) 2 minutdan keyin delete qila olmasin # 2) Comment List faqat (ish kunlari ishlasin) ( dushanbadan jumagacha)

--- myapp/permissions.py
+++ myapp/permissions.py
@@ -1,7 +1,8 @@
+from datetime import timedelta
+
 from django.utils import timezone
 from rest_framework import permissions
-from datetime import timedelta
-from .models import Comment
+
 
 class CanDeletePost(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -10,10 +11,9 @@ class CanDeletePost(permissions.BasePermission):
         if request.method == "DELETE" and request.user == obj.author:
             current_time = timezone.localtime(timezone.now())
             post_age = current_time - obj.created_at
-            has_comments = Comment.objects.filter(post=obj).exists()
-            return post_age < timedelta(minutes=2) or not has_comments
+            return post_age < timedelta(minutes=2)
         return False
 
 class IsNotWeekend(permissions.BasePermission):
     def has_permission(self, request, view):
-        return timezone.localtime(timezone.now()).weekday() not in [1, 2]
+        return timezone.localtime(timezone.now()).weekday() not in [5, 6]
