c6c77529fb2bc26b110839ccf3388fd0ff5a753e
sanjar <SanjarIsmailov@users.noreply.github.com>
2025-03-17 19:28:07 +0500

    1) 2 minutdan keyin delete qila olmasin # 2) Comment List faqat (ish kunlari ishlasin) ( dushanbadan jumagacha)

--- myapp/views.py
+++ myapp/views.py
@@ -2,40 +2,37 @@ from rest_framework import generics, permissions
 from django.shortcuts import get_object_or_404
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from .permissions import CanDeletePost, IsNotWeekend  # Import permissions
+from .permissions import CanDeletePost, IsNotWeekend
 
 class PostListCreateView(generics.ListCreateAPIView):
-    """View to list all posts or create a new post."""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsNotWeekend]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
-        """Set the author of the post to the logged-in user."""
         serializer.save(author=self.request.user)
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """View to retrieve, update, or delete a single post."""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, CanDeletePost, IsNotWeekend]
+
+    def get_permissions(self):
+        if self.request.method == "DELETE":
+            return [CanDeletePost()]
+        return [permissions.IsAuthenticatedOrReadOnly]
 
 class CommentListCreateView(generics.ListCreateAPIView):
-    """View to list all comments for a post or create a new comment."""
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsNotWeekend]
 
     def get_queryset(self):
-        """Return comments for the specified post."""
         return Comment.objects.filter(post_id=self.kwargs["post_id"])
 
     def perform_create(self, serializer):
-        """Ensure the post exists before creating a comment."""
         post = get_object_or_404(Post, id=self.kwargs["post_id"])
         serializer.save(author=self.request.user, post=post)
 
 class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """View to retrieve, update, or delete a single comment."""
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsNotWeekend]
