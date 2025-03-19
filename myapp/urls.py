14925f6779b5484bc9d82ebc4bec72fd35d49759
sanjar <SanjarIsmailov@users.noreply.github.com>
2025-03-19 22:31:03 +0500

    added jwt

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
 from .views import (
     PostListCreateView, PostDetailView,
-    CommentListCreateView, CommentDetailView
+    CommentListCreateView, CommentDetailView, JWTLoginView, JWTLogoutView
 )
 
 urlpatterns = [
@@ -12,4 +14,8 @@ urlpatterns = [
     # Comment endpoints
     path("api/posts/<int:post_id>/comments/", CommentListCreateView.as_view(), name="comment-list-create"),
     path("api/comments/<int:pk>/", CommentDetailView.as_view(), name="comment-detail"),
+
+    path('login/', JWTLoginView.as_view(), name='jwt-login'),
+    path('refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('logout/', JWTLogoutView.as_view(), name='jwt-logout'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,9 @@
 from rest_framework import generics, permissions
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import CanDeletePost, IsNotWeekend
@@ -36,3 +40,24 @@ class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsNotWeekend]
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+class JWTLoginView(TokenObtainPairView):
+    pass
+
+class JWTLogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Logged out successfully"}, status=200)
+        except Exception:
+            return Response({"error": "Invalid token"}, status=400)
